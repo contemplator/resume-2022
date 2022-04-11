80e20f71a030b07d29bd55fa3b6fd8a26b13fb13
Leo Lin <contemplation8213@gmail.com>
2022-04-11 18:11:40 +0800

    feature: first initial project 修正在 vscode 提示的錯誤語法 添加 tailwind 並測試成功

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps({
+  msg: String,
+});
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3 class="text-3xl">
+      You’ve successfully created a project with
+      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
+      <a target="_blank" href="https://vuejs.org/">Vue 3</a>. What's next?
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
